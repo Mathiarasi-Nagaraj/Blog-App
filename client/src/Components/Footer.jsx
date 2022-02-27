ddfda0f5b8ff48a5507894b54cb2b5dc3340fc94
MathiarasiNagaraj <mathiarasinagaraj05@gmail.com>
2022-02-27 21:26:38 +0530

    footer done

--- client/src/Components/Footer.jsx
+++ client/src/Components/Footer.jsx
@@ -3,39 +3,77 @@ import AppBar from '@mui/material/AppBar';
 
 import Toolbar from '@mui/material/Toolbar';
 import FacebookSharpIcon from '@mui/icons-material/FacebookSharp';
-import { IconButton } from '@mui/material';
+import {  IconButton, Typography,Box, Container } from '@mui/material';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import PinterestIcon from '@mui/icons-material/Pinterest';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+const theme = createTheme({
+  palette: {
+    primary: {
+      main:"#781D42"
+    },
+   icon:
+   {
+    main:"#FFF8F3"
+   },
+    navbar:
+    {
+      main:"#781D42"
+     
+    }
+    ,
+    logo:{
+  
+    }
+  },
+});
 export default function Footer() {
   return (
     <div>
-          <AppBar  color="primary" sx={{ top: 'auto', bottom: 0 }}>
+      <ThemeProvider theme={theme}>
+          <AppBar  color="navbar" sx={{ top: 'auto', bottom: 0,flexDirection:'column' ,alignItems: 'center'}}>
+          
         <Toolbar >
+        <Box sx={{flexDirection:'column',alignItems: 'center',display:'flex',justifyContent:'center'}}>
+    <Box>
             <IconButton
             size="large"
-            color="secondary"
+            color="icon"
             
             >
          <FacebookSharpIcon></FacebookSharpIcon>
          </IconButton>
          <IconButton
             size="large"
-            color="secondary"
+            color="icon"
             
             >
         <InstagramIcon></InstagramIcon>
          </IconButton>
          <IconButton
             size="large"
-            color="secondary"
+            color="icon"
             
             >
          <PinterestIcon></PinterestIcon>
          </IconButton>
-
-         <span>copyrights @Awsome Blossoms</span>
+         </Box>
+         <Box>
+         <Typography
+            variant="h6"
+            noWrap
+     align='center'
+            fontFamily="Lora"
+           color="wheat"
+          >
+        copyrights @Awsome Blossoms
+     </Typography>
+     </Box> 
+     </Box>
         </Toolbar>
+      
       </AppBar>
+      </ThemeProvider>
     </div>
   )
 }
